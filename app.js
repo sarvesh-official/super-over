85ad2713999a321290e86f206fdafc1b13126f04
Sarvesh <142646866+sarvesh-official@users.noreply.github.com>
2023-10-05 15:19:14 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ function updateScore() {
     team1score_tag.innerHTML = team1Score
     team1Wicket_tag.innerHTML = team1Wickets
     team2score_tag.innerHTML = team2Score
-    team2score_tag.innerHTML = team2Wickets
+    team2Wicket_tag.innerHTML = team2Wickets
 
 }
  
